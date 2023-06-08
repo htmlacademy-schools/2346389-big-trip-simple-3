d42afcf34a5f6e0c17b6959964156118950e52b0
Виталий Чаплин <vitaliychaplin@gmail.com>
2023-06-08 16:28:01 +0300

    Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Filters from './view/filters.js';
 import {render} from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter';
 import { Point } from './mock/point.js';
+import { generateFilter } from './mock/filter.js';
 
-const filterConteiner = document.querySelector('.trip-controls__filters');
 const container = document.querySelector('.trip-events');
-
 const point = new Point();
 const boardPresenter = new BoardPresenter({boardContainer: container}, point.getPoints());
-render(new Filters, filterConteiner);
+
+const filterContainer = document.querySelector('.trip-controls__filters');
+const filters = generateFilter(point.points);
+render (new Filters(filters), filterContainer);
+
 boardPresenter.init();
